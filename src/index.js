502bfb6ae51f0932f0350d55fd1440400d77ff1b
ppsamal <ppsamal99@gmail.com>
2025-04-11 10:35:42 +0530

    DB connection and utils setup

---
+++ src/index.js
@@ -0,0 +1,52 @@
+
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+import app from "./app.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("Error: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`App is running on port: ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection error", error);
+})
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+(async ()=> {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("Error: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is running on port ${process.env.PORT}`);
+        })
+    } catch(error){
+        console.log("Error: ", error);
+        throw error;
+    }
+})()
+
+*/
